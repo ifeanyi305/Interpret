06009cd856e0c7cd9478b8b6b6a2dca0e084b03f
Ifeanyi Joseph Oti <otijoseph305@gmail.com>
2023-10-20 19:06:04 +0000

    Fix font weight appearance on different browsers

--- src/pages/home/Homepage.js
+++ src/pages/home/Homepage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Homepage = () => {
   return (
     <div className="px-6">
-      <h1 className="block text-[28px] font-[800]">Home page</h1>
+      <h1 className="block text-[28px] font-[800]">Annovate</h1>
       <div className="my-4 flex items-center gap-4">
         <p className="underline">
           <Link to="/auth/signin">signin</Link>
